5207cab7eb70f3c8bafbc2c955ee7aa2301dbfe7
Seba <122824272+SebaHel@users.noreply.github.com>
2024-11-25 20:32:11 +0100

    Added Documentation Page

--- client/src/app/dashboard/Documentation/page.jsx
+++ client/src/app/dashboard/Documentation/page.jsx
@@ -3,12 +3,51 @@
 import Background from "../../../Components/Background";
 import NavBar from "../../../Components/NavBar";
 
+import styles from "../../../styles/Documentation.module.css";
+
 export default function ExercisePage() {
   return (
     <>
       <Background />
       <NavBar />
-      <div></div>
+      <div className={styles.DocContainer}>
+        <div id={styles.ParagraphContainer}>
+          <p>Syntax</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Values</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Data Types</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Strings</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Operands</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Arrays</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>Set's</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>if, elif, else</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>while</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>for</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>functions</p>
+        </div>
+        <div id={styles.ParagraphContainer}>
+          <p>classes</p>
+        </div>
+      </div>
     </>
   );
 }
