d1f009b36623e3507eb8ec7fd63eefead3d978fa
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2024-11-26 20:43:48 +0100

    Add api endpoint to receive user code

---
+++ tests_environment/api/api_endpoints.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from tests_environment.sandbox import sandbox
+
+
+app = FastAPI()
+
+
+@app.post("/execute_code/")
+async def execute_code(request: Request):
+    try:
+        data = await request.json()
+        code = data['code']
+
+        result = sandbox.run_code_and_tests(code, task_id=data['id'])
+
+        return JSONResponse(result)
+    except Exception as e:
+        return JSONResponse({"status": "error", "message": str(e)})
+
