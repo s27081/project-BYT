ae89df44b766d43272903ff61fb0a5fcb7d0a57b
Seba <122824272+SebaHel@users.noreply.github.com>
2024-12-23 20:19:58 +0100

    Adjusted Settings Page

--- client/src/app/dashboard/Settings/page.jsx
+++ client/src/app/dashboard/Settings/page.jsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
 import Background from "../../../Components/Background";
 import NavBar from "../../../Components/NavBar";
 import styles from "../../../styles/Settings.module.css";
@@ -14,13 +12,8 @@ import Modal from "../../../Components/ChangePasswordModal";
 
 export default function SettingsPage() {
   const { loading, currentUser } = useCurrentUser();
-
-  const [studentClass, setStudentClass] = useState("");
-  const [studentName, setStudentName] = useState("");
-  const [studentSurname, setStudentSurname] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const Data = { studentClass, studentName, studentSurname, currentUser };
   const router = useRouter();
 
   const openModal = () => {
@@ -44,18 +37,6 @@ export default function SettingsPage() {
     }
   };
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const response = await SignInForm(Data);
-    console.log(response);
-    if (response.success) {
-      router.push("/dashboard");
-    } else {
-      router.push("/Auth/SignIn");
-      console.error("Logownie nie powiodło się:");
-    }
-  };
-
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -71,55 +52,6 @@ export default function SettingsPage() {
         <div className={styles.SettingsBox}>
           <p id={styles.heading}>Account Information</p>
           <p>Email: {currentUser.email}</p>
-          <form onSubmit={onSubmit}>
-            <label for="name">
-              <p>
-                Name:
-                <input
-                  type="text"
-                  name="name"
-                  value={studentName}
-                  onChange={(e) => setStudentName(e.target.value)}
-                  placeholder="Enter your name"
-                  required
-                  style={{ marginLeft: "70px" }}
-                />
-              </p>
-            </label>
-
-            <label for="surname">
-              <p>
-                Surname:
-                <input
-                  type="text"
-                  name="surname"
-                  value={studentSurname}
-                  onChange={(e) => setStudentSurname(e.target.value)}
-                  placeholder="Enter your surname"
-                  required
-                  style={{ marginLeft: "15px" }}
-                />
-              </p>
-            </label>
-
-            <label for="class">
-              <p>
-                Class:
-                <input
-                  type="text"
-                  name="class"
-                  value={studentClass}
-                  onChange={(e) => setStudentClass(e.target.value)}
-                  placeholder="Enter your class"
-                  required
-                />
-              </p>
-            </label>
-
-            <button id={styles.SubmitButton} type="submit">
-              Update
-            </button>
-          </form>
           <button onClick={openModal} className={styles.delButton}>
             CHANGE PASSWORD
           </button>
