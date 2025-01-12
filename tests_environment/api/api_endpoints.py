0f8ccd5f5afc563c3c5da9d02300b9fa32552239
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-01-12 15:13:17 +0100

    Refactor API to add CORS and user_id field

--- tests_environment/api/api_endpoints.py
+++ tests_environment/api/api_endpoints.py
@@ -2,13 +2,23 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import asyncio
 from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+
 from tests_environment.sandbox import sandbox
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers (Authorization, Content-Type, etc.)
+)
 
 class CodeExecutionRequest(BaseModel):
-    id: int
+    user_id: int
+    task_id: int
     code: str
 
 
@@ -23,15 +33,16 @@ async def execute_code_with_timeout(code, task_id, timeout=5):
     except asyncio.TimeoutError:
         return {"status": "error", "message": "Code execution timed out"}
 
-
 @app.post("/execute_code/")
 async def execute_code(request: CodeExecutionRequest):
     try:
-        task_id = request.id
+        user_id = request.user_id
+        task_id = request.task_id
         code = request.code
 
         result = await execute_code_with_timeout(code, task_id, timeout=5)
 
-        return JSONResponse(result)
+        output = result.get("message", "No output received.")
+        return JSONResponse({"output": output})
     except Exception as e:
-        return JSONResponse({"status": "error", "message": str(e)})
+        return JSONResponse({"output": f"Error: {str(e)}"})
